b87b43b289512ee69c75d032d3df8a7d734a05e3
Travis Kunkel <travis.kunkel@ndus.edu>
2023-12-01 14:39:33 -0600

    Added Song model and added scaffolded item from the model, which makes the admin page complete. Added link in navbar to admin page.

---
+++ Migrations/20231201202534_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Song",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Artist = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Format = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Song", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Song");
+        }
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,14 @@
+﻿namespace MusicShop.Models
+{
+    public class Song
+    {
+        public required int ID { get; set; }
+        public required string Name { get; set; }
+        public required string Artist { get; set; }
+        public required string Format { get; set; }
+        public required string Type { get; set; }
+        public required string Genre { get; set; }
+        public required int Price { get; set; }
+
+    }
+}
