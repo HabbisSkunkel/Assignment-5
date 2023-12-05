9b68778da76727e882c2bdb4242ecb5f2cd2c162
Travis Kunkel <travis.kunkel@ndus.edu>
2023-12-05 11:09:11 -0600

    Added dropdown for Genres in BrowseMusic

---
+++ Controllers/BrowseMusicController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MusicShop.Data;
+using MusicShop.Models;
+
+namespace MusicShop.Controllers
+{
+    public class BrowseMusicController : Controller
+    {
+        private readonly MusicShopContext _context;
+
+        public BrowseMusicController(MusicShopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            ViewData["GenreId"] = new SelectList(_context.Genre, "GenreId", "GenreName");
+            return View();
+        }
+
+
+    }
+}
