c39eebe0634734b816e1d3a1c610b9bf3f2d11e0
Travis Kunkel <travis.kunkel@ndus.edu>
2023-12-05 11:34:21 -0600

    Added basic login page--does not store CartId or UserType in a cookie for now. But does make sure a user is in the OnlineUsers database. Added link to login and browse to the NavBar

---
+++ Controllers/LogonController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicShop.Data;
+
+namespace MusicShop.Controllers
+{
+    public class LogonController : Controller
+    {
+        // Add context to database
+        private readonly MusicShopContext _context;
+
+        public LogonController(MusicShopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string Username, string Password)
+        {
+            var result = (from user in _context.OnlineUser
+                         where user.UserName == Username && user.Password == Password
+                         select user).FirstOrDefault();
+
+            if (result != null)
+            {
+                // Check for admin
+                if(result.UserName =="admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "BrowseMusic");
+                }
+            }
+
+            // Not a valid login
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
