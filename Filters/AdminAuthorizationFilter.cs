1305a2e5e6e881ffa5760326bd08d929c689dc33
Travis Kunkel <travis.kunkel@ndus.edu>
2023-12-05 17:50:32 -0600

    Made change so when a user is not logged in and selects Admin they are sent to login page, unauthorized page

--- Filters/AdminAuthorizationFilter.cs
+++ Filters/AdminAuthorizationFilter.cs
@@ -8,8 +8,12 @@ namespace MusicShop
         {
             // Check if user is admin
             context.HttpContext.Request.Cookies.TryGetValue("UserType", out string? userType);
-
-            if (userType != "admin")
+            if (userType == null)
+            {
+                // redirect to login
+                context.Result = new Microsoft.AspNetCore.Mvc.RedirectToActionResult("Index", "Logon", null);
+            }
+            else if (userType != "admin")
             {
                 // redirect to Unauthorized page
                 context.Result = new Microsoft.AspNetCore.Mvc.RedirectToActionResult("Index", "Unauthorized", null);
