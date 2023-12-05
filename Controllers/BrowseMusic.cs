42e19e15b790b5f1712c274a895283331b22d2f1
vogtk <vogtk@LADICUS>
2023-12-05 10:58:17 -0600

    Added BrowseMusic Controller & View

---
+++ Controllers/BrowseMusic.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicShop.Models;
+
+namespace MusicShop.Controllers
+{
+    public class BrowseMusic : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
